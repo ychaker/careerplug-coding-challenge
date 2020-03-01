fa04aeb2f174fdf3d914dc05979b1d9849c15a8f
Youssef Chaker <youssef@bearandgiraffe.com>
2020-03-01 16:28:33 -0600

    Adds the basic Post CRUD.
    In this commit we are able to create new posts,
    list posts, delete posts, and view posts.
    
    We also use the Trix WYSIWYG editor to create rich text
    bodies.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,43 @@
 
 # Posts controller
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show destroy ]
+
   def index
-    # Return all `Post`
+    # Fetch all posts in reverse chronological order
+    @posts = Post.order(created_at: :desc).all
   end
 
   def new
-    # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
-    # Add a new `Post` to the database
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = t('.success')
+      redirect_to @post
+    else
+      flash[:error]  = t('.error')
+      render :new
+    end
   end
 
   def destroy
-    # Remove a `Post` from the database
+    @post.destroy
+
+    flash[:notice] = t('.success')
+    redirect_to root_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  # Validations
+  validates :title,
+    presence: true
+
+  validates :body,
+    presence: true
 end
